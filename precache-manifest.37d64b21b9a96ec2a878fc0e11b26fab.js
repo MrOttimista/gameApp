ed0003e2c2e6e416cae22d427cac1ec5fb0882e5
adel <adel.mohamed855@gmail.com>
2019-03-12 17:22:39 +0200

    Updates

---
+++ precache-manifest.37d64b21b9a96ec2a878fc0e11b26fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gameApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b54e20dbd466b4cf732c",
+    "url": "/gameApp/static/js/runtime~main.b54e20db.js"
+  },
+  {
+    "revision": "20f6e4ab5a08803db641",
+    "url": "/gameApp/static/js/main.20f6e4ab.chunk.js"
+  },
+  {
+    "revision": "e9e1ecadedd80cb987e0",
+    "url": "/gameApp/static/js/1.e9e1ecad.chunk.js"
+  },
+  {
+    "revision": "20f6e4ab5a08803db641",
+    "url": "/gameApp/static/css/main.6942874e.chunk.css"
+  },
+  {
+    "revision": "e9e1ecadedd80cb987e0",
+    "url": "/gameApp/static/css/1.5c876472.chunk.css"
+  },
+  {
+    "revision": "96dd254fb13762f9897d0ecccc463b08",
+    "url": "/gameApp/index.html"
+  }
+];
